2c5bb5f152308d683e28fc5b8654a6dfa32e3be8
Parker Hammond <339720419@GoTVDSB.ca>
2024-09-27 08:36:57 -0400

    Add project files.

---
+++ topic 5 part 2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace topic_5_part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int secerectnumber;
+            secerectnumber = 1;
+
+
+
+            Console.WriteLine("guess a number between 1-10");
+            int.TryParse(Console.ReadLine(), out number);
+            if (number == 1) 
+            {
+
+                Console.WriteLine("omg you got it right");
+
+
+
+            }
+            else
+            {
+
+
+                Console.WriteLine("thats not it the number was " + secerectnumber );
+
+            }
+            
+
+        }
+    }
+}
